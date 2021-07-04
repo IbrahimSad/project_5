42699ac82e6b05f86612dd1209b897b3878b8296
IbrahimSad <77147115+IbrahimSad@users.noreply.github.com>
2021-07-03 19:58:40 -0700

    last project_5 now

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+/* globel $ ,alert ,console*/
+$(function () {
+
+    'use strict';
+
+    // Simple mode, it styles document scrollbar:
+
+    $("body").niceScroll({
+
+        cursorcolor: "crimson",
+
+        cursorborder: "1px solid crimson",
+    });
+});
